8fbec67ddd715202415fd22dbb1f6bb7d87a835d
Ankita <lisamunshi1616@gmail.com>
2024-07-29 14:58:00 +0530

    first commit

---
+++ src/main/java/com/jsp/spring_thyme/SpringThymeApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.spring_thyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/spring_thyme/SpringThymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.spring_thyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
